1796b8f91937cfaf5eb8dac1e1b60bed12fdea9a
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-25 16:36:48 -0600

    added outputs

--- VPC/outputs.tf
+++ VPC/outputs.tf
@@ -1,12 +1,11 @@
-
-output vpc_id {
-  value       = module.vpc.vpc_id
+output "vpc_ids" {
+  value = concat([for vpc in module.vpc : vpc.vpc_id], [for vpc in module.vpc-california : vpc.vpc_id])
 }
 
-output private_subnets {
-  value       = module.vpc.private_subnets
+output "private_subnets" {
+  value = concat([for vpc in module.vpc : vpc.private_subnets], [for vpc in module.vpc-california : vpc.private_subnets])
 }
 
-output public_subnets {
-  value       = module.vpc.public_subnets
+output "public_subnets" {
+  value = concat([for vpc in module.vpc : vpc.public_subnets], [for vpc in module.vpc-california : vpc.public_subnets])
 }
