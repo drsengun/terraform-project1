52ac9e7540c5c300453e8ca1d46e1da6842be799
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-24 20:21:48 -0600

    test

--- VPC/main.tf
+++ VPC/main.tf
@@ -33,7 +33,7 @@ module "vpc" {
 
 module "vpc-california" {
   source = "terraform-aws-modules/vpc/aws"
-  count = var.region == "california" ? 1 : 0
+  count = var.region == "california" ? 0 : 1
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
