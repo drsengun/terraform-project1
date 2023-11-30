4e9418c72bd9464ebb2750070633ecd3b4390561
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-29 21:36:50 -0600

    bb

---
+++ ASG/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "company-wide-storage-illiasoroka"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
---
+++ RDS/main.tf
@@ -0,0 +1,74 @@
+# Create a RDS
+resource "aws_db_subnet_group" "db_sub_group" {
+  name = "db_sub_group"
+  subnet_ids = (module.vpc.private_subnets)
+}
+
+# Create DB instance
+resource "aws_db_instance" "db_writer" {
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "mysql"
+  engine_version = "8.0.28"
+  instance_class = "db.t2.micro"
+  identifier = "db-writer"
+  username = "admin"
+  password = random_string.rds_password.result
+  db_subnet_group_name = aws_db_subnet_group.db_sub_group.name
+  vpc_security_group_ids = [aws_security_group.rds_allow_rule.id]
+  skip_final_snapshot = true
+  publicly_accessible = true
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes = all
+  }
+}
+
+resource "aws_db_instance" "db_readers" {
+  count = 3
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "mysql"
+  engine_version = "8.0.28"
+  instance_class = "db.t2.micro"
+  username = "admin"
+  identifier                   = "db-reader-${count.index + 1}"
+  password = random_string.rds_password.result
+  db_subnet_group_name = aws_db_subnet_group.db_sub_group.name
+  vpc_security_group_ids = [aws_security_group.rds_allow_rule.id]
+  skip_final_snapshot = true
+  publicly_accessible = true
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes = all
+  }
+}
+
+resource "random_string" "rds_password" {
+  length  = 16
+  special = false
+}
+
+resource "aws_security_group" "rds_allow_rule" {
+  description = "Allow port 3306"
+  vpc_id      = module.vpc.vpc_ids
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    #security_groups = [ "${module.vpc.default_security_group_id}" ]
+  }
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+module "vpc" {
+  source = "../VPC/"
+}
